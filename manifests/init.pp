a5142308fe3c6b1ee2dc3c64ca1e99725c65133f
Nick Pellant <nick@nickpellant.com>
2014-07-12 00:49:15 +0100

    Bump Plex source version to latest release.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,6 @@
 class plex {
   package { 'Plex':
     provider => 'compressed_app',
-    source   => 'http://plex.r.worldssl.net/plex-laika/0.9.5.4/Plex-0.9.5.4-973998f.zip'
+    source   => 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/PlexMediaServer-0.9.9.12.504-3e7f93c-OSX.zip'
   }
 }
